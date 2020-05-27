790f5a2a2598cf740e4c7061f65bf699640b0fea
guoqiaomixian <2407241029@qq.com>
2020-05-27 19:01:01 +0800

    原始框架，已经完成上传地点的json文档，并在后台获取文档

---
+++ school.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request,flash,redirect,url_for
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','json'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def hello_world():
+    # return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    print("------okok----okok")
+    return ''
+
+if __name__ == '__main__':
+    app.run()
